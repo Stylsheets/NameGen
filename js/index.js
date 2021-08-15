723959b6de333e63f926344310b82177b5ecd485
Fatih Yıldız <0m.fatih.yildiz@gmail.com>
2021-08-15 09:52:38 +0300

    responsive edit

--- js/index.js
+++ js/index.js
@@ -60,7 +60,7 @@ function create5Words(maxLength, minLength, startsWith) {
 
     span.innerHTML = words[i];
     li.appendChild(span);
-    button.className = 'transition text-gray-300 active:text-gray-800 hover:text-gray-800';
+    button.className = 'transition text-gray-300 active:text-gray-800 sm:hover:text-gray-800';
     button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
       <path d="M9 2a2 2 0 00-2 2v8a2 2 0 002 2h6a2 2 0 002-2V6.414A2 2 0 0016.414 5L14 2.586A2 2 0 0012.586 2H9z" />
       <path d="M3 8a2 2 0 012-2v10h8a2 2 0 01-2 2H5a2 2 0 01-2-2V8z" />
